b48ae48a728d3eb67f7a89557df28735b63a767e
Salaheldinyoussry <61039282+Salaheldinyoussry@users.noreply.github.com>
2021-10-01 09:34:09 +0200

    commit

--- customTracker.js
+++ customTracker.js
@@ -93,10 +93,9 @@
     //   'Content-Type': 'application/json'
     //   // 'Content-Type': 'application/x-www-form-urlencoded',
     // },
-      credentials: 'omit'
-,
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+  //    credentials: 'omit',
+   // redirect: 'follow', // manual, *follow, error
+    //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
     body: data// body data type must match "Content-Type" header
   });
 
